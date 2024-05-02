f3ad8ba112146d4672620fe8d0dc71d505479d9e
szvitek <gy.szvitek@gmail.com>
2024-05-02 01:27:59 +0100

    init(HomePage): home page content + styling

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,26 @@
+import Image from 'next/image';
+import preview from '../../../public/petsoft-preview.png';
+import Logo from '@/components/logo';
+
 export default function Home() {
-  return <main>home</main>;
+  return (
+    <main className="bg-[#5dc9a8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
+      <Image src={preview} alt="Preview of PetSoft" />
+      <div>
+        <Logo />
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">
+          Manage your <span className="font-extrabold">pet daycare</span> with
+          ease
+        </h1>
+        <p className="text-2xl font-medium max-w-[600px]">
+          Use PetSoft to easily keep track of your pets under your daycare. Get
+          lifetime access for $299.
+        </p>
+        <div className="mt-10">
+          <button>Get Started</button>
+          <button>Log In</button>
+        </div>
+      </div>
+    </main>
+  );
 }
