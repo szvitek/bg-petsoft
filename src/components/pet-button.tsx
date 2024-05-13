ff8720a713632f0078899f651a4745e3c9d664d0
szvitek <gy.szvitek@gmail.com>
2024-05-14 00:43:03 +0100

    feat(Dialog): shadcn-ui dialog for adding and editing pet

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,6 +1,14 @@
 import { PlusIcon } from '@radix-ui/react-icons';
 import { Button } from './ui/button';
 import { PropsWithChildren } from 'react';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
+import PetForm from './pet-form';
 
 type PetButtonProps = {
   actionType: 'add' | 'edit' | 'checkout';
@@ -12,18 +20,6 @@ export default function PetButton({
   children,
   onClick,
 }: PropsWithChildren<PetButtonProps>) {
-  if (actionType === 'add') {
-    return (
-      <Button size="icon">
-        <PlusIcon className="h-6 w-6" />
-      </Button>
-    );
-  }
-
-  if (actionType === 'edit') {
-    return <Button variant="secondary">{children}</Button>;
-  }
-
   if (actionType === 'checkout') {
     return (
       <Button variant="secondary" onClick={onClick}>
@@ -31,4 +27,26 @@ export default function PetButton({
       </Button>
     );
   }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        {actionType === 'add' ? (
+          <Button size="icon">
+            <PlusIcon className="h-6 w-6" />
+          </Button>
+        ) : (
+          <Button variant="secondary">{children}</Button>
+        )}
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {actionType === 'add' ? 'Add a new pet' : 'Edit pet'}
+          </DialogTitle>
+        </DialogHeader>
+        <PetForm />
+      </DialogContent>
+    </Dialog>
+  );
 }
