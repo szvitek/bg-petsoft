5dbb6a09280daac5bc21386ac0d7bcfc1b16c84a
szvitek <gy.szvitek@gmail.com>
2024-06-05 01:10:50 +0100

    feat(Auth): display user email on the UI

--- src/app/(app)/app/account/page.tsx
+++ src/app/(app)/app/account/page.tsx
@@ -1,12 +1,19 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from '@/lib/auth'
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+  if (!session?.user) {
+    redirect('/login')
+  }
+
   return <main>
     <H1 className="my-8 text-white">Your account</H1>
 
     <ContentBlock className="h-[500px] flex justify-center items-center">
-      <p>Logged in as ...</p>
+      <p>Logged in as { session.user.email }</p>
     </ContentBlock>
   </main>;
 }
